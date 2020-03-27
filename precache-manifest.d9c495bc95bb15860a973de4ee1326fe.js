a57fbaef744d22ec7e1418f98563352d29b87078
Kamil Skorzynski <skorzynskikamil@gmail.com>
2020-03-27 17:44:36 +0100

    Updates

---
+++ precache-manifest.d9c495bc95bb15860a973de4ee1326fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbb8cae8c8ec20e82370",
+    "url": "/Web-React/static/css/main.0e255fa2.chunk.css"
+  },
+  {
+    "revision": "cbb8cae8c8ec20e82370",
+    "url": "/Web-React/static/js/main.f59d3bb3.chunk.js"
+  },
+  {
+    "revision": "1bcae934622e7955b397",
+    "url": "/Web-React/static/js/runtime~main.87bbb4d8.js"
+  },
+  {
+    "revision": "59d34577f5539b54518e",
+    "url": "/Web-React/static/js/2.09c84cda.chunk.js"
+  },
+  {
+    "revision": "d0a9d96be4ef6d8388c588d6b152ac29",
+    "url": "/Web-React/static/media/sport4.d0a9d96b.jpg"
+  },
+  {
+    "revision": "ec1c2acd891768357d4206d9014224bc",
+    "url": "/Web-React/static/media/sport1.ec1c2acd.jpg"
+  },
+  {
+    "revision": "1ec7df8822db773851e038c280b405fd",
+    "url": "/Web-React/static/media/sport2.1ec7df88.jpg"
+  },
+  {
+    "revision": "b99133f32f93f618b7a3220689d77562",
+    "url": "/Web-React/static/media/sport3.b99133f3.jpg"
+  },
+  {
+    "revision": "b719d2073399c12d3a08df1c99dbdfd5",
+    "url": "/Web-React/index.html"
+  }
+];
